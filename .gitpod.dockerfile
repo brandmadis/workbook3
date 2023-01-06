626c54c951553823fe2e76d45eda8a94fcf1b5f0
Brandon Madison <brandmadis@gmail.com>
2023-01-06 19:15:37 +0000

    Init postgres

---
+++ .gitpod.dockerfile
@@ -0,0 +1,3 @@
+FROM gitpod/workspace-postgresql
+
+USER gitpod
--- mysite/settings.py
+++ mysite/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASE = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dev-project',
+        'USER': 'djangodev',
+        'PASSWORD': 'djangodev',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

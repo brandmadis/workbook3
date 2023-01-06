af908533db8005bcafdef156f4ab685dfdb81d80
Brandon Madison <brandmadis@gmail.com>
2023-01-06 21:28:52 +0000

    Init

--- exercises/views.py
+++ exercises/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+def index(request):
+    TestModel.objects.create(name="name")
+    all_names = TestModel.objects.all()
+    return HttpResponse(all_names)

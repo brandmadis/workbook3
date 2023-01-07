c6f1434ed51b7db32cf7d886512ce970cf8c1d93
Brandon Madison <brandmadis@gmail.com>
2023-01-07 00:29:15 +0000

    Add exercises files

--- exercises/models.py
+++ exercises/models.py
@@ -3,4 +3,96 @@ from django.db import models
 class TestModel(models.Model):
     name = models.CharField(max_length=200)
 
+# -*- coding: utf-8 -*-
+# from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    superpower = models.CharField(max_length=100)
+    is_good = models.BooleanField()
+    is_male = models.BooleanField()
+    rating = models.IntegerField(blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
     
+    def __str__(self):
+        return self.name
+        
+        
+
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    birthplace = models.CharField(max_length=200)
+    is_male = models.BooleanField()
+    
+    def __str__(self):
+        return self.name 
+        
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    
+    def __str__(self):
+        return self.make + " " + self.model + " " + str(self.year)
+    
+class Stock(models.Model):
+    company = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    market_cap = models.FloatField()
+    
+    def __str__(self):
+        return self.company
+        
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+        
+    
+class Song(models.Model):
+    name = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    population = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+        
+    
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+
+class NBATeam(models.Model):
+    name = models.CharField(max_length=255)
+    worth = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+        
+    
+class NBAAthlete(models.Model):
+    name = models.CharField(max_length=255)
+    team = models.ForeignKey(NBATeam, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
--- exercises/views.py
+++ exercises/views.py
@@ -6,3 +6,46 @@ def index(request):
     TestModel.objects.create(name="name")
     all_names = TestModel.objects.all()
     return HttpResponse(all_names)
+
+def superheroes(request):
+    # code below!
+    superheroes = Superhero.objects.filter(name="wolverine")
+    context = { 'superheroes': superheroes }
+    return render(request, 'exercises/superhero.html', context) 
+    
+
+def animals(request):
+    animals = None
+    # code below!
+    context = { 'animals': animals }
+    return render(request, 'exercises/animal.html', context)       
+
+def cars(request):
+    cars = None
+    # code below!
+    cars = Car.objects.all()
+    context = { 'cars': cars }
+    return render(request, 'exercises/car.html', context)       
+
+
+def templates(request):
+    superheroes = Superhero.objects.all()
+    context = { 'superheroes': superheroes }
+    return render(request, 'exercises/template.html', context)       
+
+def summary(request):
+    
+    context = {  }
+    return render(request, 'exercises/summary.html', context)       
+    
+def submit_form(request):
+    context = { }
+    return render(request, "exercises/submit_form.html", context)
+    
+def team_edward(request):
+    return HttpResponse(request.POST['message'] + " is a fervent supporter of team edward")
+    
+def team_jacob(request):
+    return HttpResponse(request.POST['message'] + " is a member of team jacob. ROAR!!")
+    
+    
